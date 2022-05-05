e40e7214650b6d437ba2f8ece94fc247e8aec561
erkm <erkm@LEAP-02s-iMac.local>
2022-05-05 18:46:01 +0800

    Feature - Create button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,72 @@
+import React, {ReactNode} from 'react';
+import {Typography} from './Typography';
+import {colors} from './Colors';
+import {View, StyleSheet, Pressable, PressableProps} from 'react-native';
+type ButtonType = PressableProps & {
+  size?: 'L' | 'S';
+  disabled?: boolean;
+  iconRight?: ReactNode;
+  iconLeft?: ReactNode;
+  type?: 'primary' | 'secondary';
+};
+export const Button: React.FC<ButtonType> = ({
+  onPress,
+  disabled = false,
+  type = 'primary',
+  size = 'S',
+  iconRight = false,
+  iconLeft = false,
+  children,
+}) => {
+  return (
+    <Pressable
+      onPress={onPress}
+      disabled={disabled}
+      style={[
+        styles.button,
+        styles[size],
+        disabled && styles.disabled,
+        styles[type],
+      ]}>
+      <View style={{width: iconLeft != undefined ? 20 : 0}}>{iconLeft}</View>
+      <Typography
+        type={type === 'primary' ? 'body_bold' : 'body_regular'}
+        color={type === 'secondary' ? 'primary' : 'white'}
+        children={children}
+      />
+      <View style={{width: iconRight != undefined ? 20 : 0}}>{iconRight}</View>
+    </Pressable>
+  );
+};
+const styles = StyleSheet.create({
+  button: {
+    alignItems: 'center',
+    borderRadius: 4,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    borderWidth: 1,
+  },
+  S: {
+    height: 38,
+    width: 124,
+    paddingHorizontal: 20,
+  },
+  L: {
+    height: 48,
+    width: 132,
+    paddingHorizontal: 20,
+  },
+  disabled: {
+    opacity: 0.5,
+  },
+  primary: {
+    backgroundColor: colors.primary,
+    borderColor: '#EAEAEA',
+  },
+  secondary: {
+    backgroundColor: colors.white,
+    borderColor: '#FF7C76',
+  },
+});
+
+export default Button;
