86c70241264eb7d3c3c71f85e5148a9c4fec1852
mkar <mkar@27s-iMac.local>
2022-05-05 18:34:06 +0800

    Theme

---
+++ src/components/theme.ts
@@ -0,0 +1,16 @@
+export const Theme = {
+  palette: {
+    primary: {
+      red: '#FF7C76',
+      blue: '#80E0F1',
+    },
+    onboarding: {
+      opacButton: '#FFCFCC',
+      flowRed: '#EDB3AD',
+      flowBlue: '#80E0F1',
+    },
+    background: {
+      default: '#FAFAFA',
+    },
+  },
+};
