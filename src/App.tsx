d3dc7db565331c3da54d5c1aac31782cec24b5c3
erkm <erkm@LEAP-02s-iMac.local>
2022-05-05 18:46:01 +0800

    Feature - Create button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import {SafeAreaView, StyleSheet, Text} from 'react-native';
+import {Button} from './components';
+import RightArrow from './assets/RightArrow.svg';
 const App = () => {
   return (
     <SafeAreaView style={styles.container}>
       <Text>Hello World</Text>
+      <Button size="L" disabled>
+        asd
+      </Button>
     </SafeAreaView>
   );
 };
