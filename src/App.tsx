8db9ab840bee31a6dcb650a3c93c882b3d1595db
erkm <erkm@LEAP-02s-iMac.local>
2022-05-07 12:36:34 +0800

    Feature - Button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,9 @@
 import React from 'react';
 import {SafeAreaView, StyleSheet, Text} from 'react-native';
-import {Button} from './components';
-import RightArrow from './assets/svgs/RightArrow.svg';
 const App = () => {
   return (
     <SafeAreaView style={styles.container}>
       <Text>Hello World</Text>
-      <Button size="L" disabled>
-        asd
-      </Button>
     </SafeAreaView>
   );
 };
