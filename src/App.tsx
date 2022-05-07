beec394fdd69e0406c4a7039b21736ef8d41f654
Enkhmunkh <63009299+Enhmunh-E@users.noreply.github.com>
2022-05-07 10:55:28 +0800

    Feature -  Created Calendar Component (#22)
    * Feature -  Created Calendar Component
    
    * Feature - ignored Podfile and yarn lock

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
-import {SafeAreaView, Text} from 'react-native';
+import {SafeAreaView, StyleSheet, Text} from 'react-native';
 const App = () => {
   return (
-    <SafeAreaView>
+    <SafeAreaView style={styles.container}>
       <Text>Hello World</Text>
     </SafeAreaView>
   );
 };
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+  },
+});
 export default App;
