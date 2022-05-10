17f112f4717a704c71eefed5eba51291673d55c7
erkm <erkm@LEAP-02s-iMac.local>
2022-05-10 20:01:56 +0800

    Feature: Login

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -2,7 +2,9 @@
 import React from 'react';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import BottomTabs from '../navigation/BottomTabs';
+import LoginScreen from '../screens/LoginScreen';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Header from '../components/Header';
 
 const Stack = createNativeStackNavigator();
 
@@ -10,6 +12,11 @@ export const AppNavigator = () => {
   return (
     <SafeAreaView style={{flex: 1}}>
       <Stack.Navigator initialRouteName="Content">
+        <Stack.Screen
+          name="Login"
+          component={LoginScreen}
+          options={{header: () => <Header icon={false} />}}
+        />
         <Stack.Screen
           name="Content"
           component={BottomTabs}
--- src/screens/types.tsx
+++ src/screens/types.tsx
@@ -2,4 +2,6 @@ export type RootStackParamList = {
   HomeScreen: undefined;
   CalendarScreen: undefined;
   InformationScreen: undefined;
+  Content: undefined;
+  Login: undefined;
 };
