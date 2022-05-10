c1cd3996e6f69a001fc7a2cc466fc26d0f6e00c6
Enhmunh-E <enhuush0704@gmail.com>
2022-05-10 19:08:42 +0800

    Feature - Added Symptom Visible show and Provider for userId, added symptom adding function

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,17 @@ import React from 'react';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import {NavigationContainer} from '@react-navigation/native';
 import {AppNavigator} from './navigation/AppNavigator';
+import {AuthProvider} from './providers/AuthProvider';
+import {Provider} from './providers/Provider';
 const App = () => {
   return (
     <NavigationContainer>
       <SafeAreaProvider>
-        <AppNavigator />
+        <AuthProvider>
+          <Provider>
+            <AppNavigator />
+          </Provider>
+        </AuthProvider>
       </SafeAreaProvider>
     </NavigationContainer>
   );
