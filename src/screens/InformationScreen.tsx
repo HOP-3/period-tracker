e97c83fd5af0b19d62c8072b727922ee399c13d2
erkm <erkm@LEAP-02s-iMac.local>
2022-05-12 15:59:32 +0800

    --amend

--- src/screens/InformationScreen.tsx
+++ src/screens/InformationScreen.tsx
@@ -1,13 +1,9 @@
 import React from 'react';
-
 import {View, Text} from 'react-native';
-import auth from '@react-native-firebase/auth';
-import {Button} from '../components';
 
 export const InformationScreen = () => {
   return (
     <View>
-      <Button onPress={() => auth().signOut()}>tap</Button>
       <Text>Information Screen</Text>
     </View>
   );
