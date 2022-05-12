14f9bf15b69839a4716b61c6f004b198b03e0f1e
erkm <erkm@LEAP-02s-iMac.local>
2022-05-12 18:13:53 +0800

    Improvement: Login Error Code

--- src/screens/InformationScreen.tsx
+++ src/screens/InformationScreen.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import {View, Text} from 'react-native';
+import auth from '@react-native-firebase/auth';
+import {Button} from '../components';
 
 export const InformationScreen = () => {
   return (
     <View>
       <Text>Information Screen</Text>
+      <Button onPress={() => auth().signOut()}>asd</Button>
     </View>
   );
 };
