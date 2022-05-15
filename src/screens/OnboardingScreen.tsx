b0f13cd26a65b7d76c56580b0dd30423ca29d56a
Zenobldd <oyunbold@callpro.mn>
2022-05-15 20:37:23 +0800

    animation error

--- src/screens/OnboardingScreen.tsx
+++ src/screens/OnboardingScreen.tsx
@@ -22,22 +22,30 @@ type DataType = {
   description: string;
   title: string;
 };
+
 type NavigationProps = NativeStackNavigationProp<RootStackParamList>;
 
 export const OnBoarding = () => {
   const navigation = useNavigation<NavigationProps>();
   const [imageIndex, setImageIndex] = useState(0);
   const fadeAnim = useRef(new Animated.Value(1)).current;
+  const translation = useRef(new Animated.ValueXY({x: 0, y: 0})).current;
+  const translation1 = useRef(new Animated.ValueXY({x: 800, y: -100})).current;
+  const translation2 = useRef(new Animated.ValueXY({x: 0, y: 1000})).current;
+  const translation3 = useRef(new Animated.ValueXY({x: -800, y: 100})).current;
+
   const indexPlus = () => {
     if (imageIndex + 1 === data.length) {
       navigation.navigate('Content');
     } else {
-      fadeOut();
-
+      imageAnimationP();
+      textAnimation();
       setImageIndex(imageIndex + 1);
     }
   };
   const indexMinus = () => {
+    imageAnimationN();
+    textAnimation();
     setImageIndex(imageIndex - 1);
   };
 
@@ -69,27 +77,63 @@ export const OnBoarding = () => {
       title: 'Аяллаа хамтдаа эхэлцгээе!',
     },
   ];
-  const fadeIn = () => {
-    // Will change fadeAnim value to 1 in 5 seconds
-    Animated.timing(fadeAnim, {
-      toValue: 1,
-      duration: 500,
-      useNativeDriver: true,
-    }).start();
+  const textAnimation = () => {
+    Animated.sequence([
+      Animated.timing(fadeAnim, {
+        toValue: 0,
+        duration: 200,
+        useNativeDriver: true,
+      }),
+      Animated.timing(fadeAnim, {
+        toValue: 1,
+        duration: 600,
+        useNativeDriver: true,
+      }),
+    ]).start();
   };
-  const fadeOut = () => {
-    Animated.timing(fadeAnim, {
-      toValue: 0,
-      duration: 100,
-      useNativeDriver: true,
-    }).start()
-    Animated.timing(fadeAnim, {
-      toValue: 1,
-      duration: 1000,
-      useNativeDriver: true,
-    }).start();
+  const imageAnimationP = () => {
+    console.log(imageIndex);
+    Animated.parallel([
+      Animated.timing(translation, {
+        toValue: {
+          x: imageIndex === 0 ? -800 : imageIndex === 1 ? 0 : 800,
+          y: imageIndex === 0 ? 100 : imageIndex === 1 ? 1000 : -100,
+        },
+        duration: 500,
+        useNativeDriver: true,
+      }),
+      Animated.timing(translation1, {
+        toValue: {
+          x: imageIndex === 0 ? 0 : imageIndex === 1 ? -800 : 0,
+          y: imageIndex === 0 ? -370 : imageIndex === 1 ? 100 : 1000,
+        },
+        duration: 500,
+        useNativeDriver: true,
+      }),
+      Animated.timing(translation2, {
+        toValue: {
+          x: imageIndex === 0 ? 800 : imageIndex === 1 ? 0 : -800,
+          y: imageIndex === 0 ? -370 : imageIndex === 1 ? -740 : 100,
+        },
+        duration: 500,
+        useNativeDriver: true,
+      }),
+    ]).start();
   };
-
+  const imageAnimationN = () => {
+    Animated.parallel([
+      Animated.timing(translation1, {
+        toValue: {x: 400, y: -200},
+        duration: 500,
+        useNativeDriver: true,
+      }),
+      Animated.timing(translation, {
+        toValue: {x: 0, y: 0},
+        duration: 500,
+        useNativeDriver: true,
+      }),
+    ]).start();
+  };
   return (
     <View style={styles.container}>
       <View style={{alignSelf: 'flex-end', marginRight: 10}}>
@@ -100,6 +144,37 @@ export const OnBoarding = () => {
           Алгасах
         </Button>
       </View>
+      <View style={{maxHeight: 380, borderWidth: 1}}>
+        <Animated.Image
+          style={{
+            transform: [
+              {translateX: translation.x},
+              {translateY: translation.y},
+            ],
+          }}
+          source={require('../assets/pngs/first.png')}
+        />
+
+        <Animated.Image
+          source={require('../assets/pngs/second.png')}
+          style={{
+            transform: [
+              {translateX: translation1.x},
+              {translateY: translation1.y},
+            ],
+          }}
+        />
+        <Animated.Image
+          style={{
+            transform: [
+              {translateX: translation2.x},
+              {translateY: translation2.y},
+            ],
+          }}
+          source={require('../assets/pngs/third.png')}
+        />
+      </View>
+
       {/* <Image source={imageData[imageIndex]} /> */}
       <Animated.View style={{opacity: fadeAnim}}>
         <Text style={styles.title}>{data[imageIndex].title}</Text>
